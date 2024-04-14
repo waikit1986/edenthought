33c6e44151e5700d1142641d4ac5cc110dbd4436
waikit <waikit6461@gmail.com>
2024-04-14 20:15:00 +0800

    update

--- edenthought/settings.py
+++ edenthought/settings.py
@@ -18,9 +18,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['edenthought-nt3n.onrender.com', '*']
 
-# CSRF_TRUSTED ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ['https://edenthought-nt3n.onrender.com']
 
 
 
